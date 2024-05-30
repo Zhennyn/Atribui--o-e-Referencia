a1c090fbb2bae9bc58aee5dd97b770b7c8a3aef7
Zhennyn <matheuslimamenezes2005@gmail.com>
2024-05-30 10:45:25 -0300

    first commit

---
+++ src/test/java/com/projeto/atribuicaoereferencia/AppTest.java
@@ -0,0 +1,20 @@
+package com.projeto.atribuicaoereferencia;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
